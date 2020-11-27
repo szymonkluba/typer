8795750beb766cd31b971c2f96bc74e5920521b5
Szymon Kluba <szymonkluba@gmail.com>
2020-11-27 23:25:16 +0100

    Removed old database reference.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,7 +8,6 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
         PONY={
             'provider': 'sqlite',
             'filename': 'flaskr.sqlite',
@@ -32,9 +31,6 @@ def create_app(test_config=None):
     @app.route("/hello")
     def hello():
         return 'Hello, World'
-
-    from . import db
-    db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
