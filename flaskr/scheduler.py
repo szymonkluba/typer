f3869aa343899f2080a9ff35c50f94ba688b7a18
Szymon Kluba <szymonkluba@gmail.com>
2020-12-04 18:41:54 +0100

    Schedules for checking qualifications and participants.

--- flaskr/scheduler.py
+++ flaskr/scheduler.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 sys.path.append("/home/szymonkluba/mysite/typer/")
 
-from flaskr.fis_scraping import get_results, check_new_tournaments, check_tournament_updates
+from flaskr.fis_scraping import get_results, check_new_tournaments, check_tournament_updates, get_participants
 import flaskr.pony_db as pony_db
 
 
@@ -53,9 +53,16 @@ def tournament_updates():
     check_tournament_updates()
 
 
+@with_logging
+def participants(qualifications):
+    get_participants(qualifications)
+
+
+
 now = datetime.now()
 with db_session:
     current_tournament = pony_db.get_tournament_by_status("następne")
+    qualifications = pony_db.select_qualifications_by_date(now)
 t_date_time = current_tournament.date_time + timedelta(hours=1)
 if now.year == t_date_time.year and now.month == t_date_time.month and now.day == t_date_time.day:
     schedule.every().day.at(datetime.strftime(t_date_time, "%H:%M")).do(close_tournament,
@@ -76,6 +83,13 @@ schedule.every().day.at("09:15").do(new_tournaments)
 print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking of new tournaments', flush=True)
 schedule.every().day.at("02:00").do(kill_task)
 print(f'{datetime.now().strftime("%H:%M")} - Scheduled kill task', flush=True)
+if qualifications:
+    schedule.every().day.at("13:00").do(participants, qualifications=qualifications)
+    print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking of tournaments updates', flush=True)
+    for i in range(1, 13):
+        time_schedule = f'{13 + (i % 4)}:{15 * (i % 4)}'
+        schedule.every().day.at(time_schedule).do(participants, qualifications=qualifications)
+        print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking of tournaments updates', flush=True)
 if schedule.jobs:
     for job in schedule.jobs:
         print(job)
