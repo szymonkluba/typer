7cbcf7ad029da8ed6f0a7e9db7c303dc1ebf4c8f
Szymon Kluba <szymonkluba@gmail.com>
2020-12-04 22:21:00 +0100

    Scheduler

--- flaskr/scheduler.py
+++ flaskr/scheduler.py
@@ -63,13 +63,14 @@ now = datetime.now()
 with db_session:
     current_tournament = pony_db.get_tournament_by_status("następne")
     qualifications = pony_db.select_qualifications_by_date(now)
-if qualifications:
-    schedule.every().day.at("13:00").do(participants, qualifications=qualifications).tag('checking_participants')
-    print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking of tournaments updates', flush=True)
-    for i in range(1, 13):
-        time_schedule = f'{13 + (i % 4)}:{15 * (i % 4)}'
-        schedule.every().day.at(time_schedule).do(participants, qualifications=qualifications).tag(
-                'checking_participants')
+with db_session:
+    if qualifications:
+        schedule.every().day.at("13:00").do(participants, qualifications=qualifications).tag('checking_participants')
+        print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking of tournaments updates', flush=True)
+        for i in range(1, 13):
+            time_schedule = f'{13 + (i % 4)}:{15 * (i % 4)}'
+            schedule.every().day.at(time_schedule).do(participants, qualifications=qualifications).tag(
+                    'checking_participants')
 t_date_time = current_tournament.date_time + timedelta(hours=1)
 if now.year == t_date_time.year and now.month == t_date_time.month and now.day == t_date_time.day:
     schedule.every().day.at(datetime.strftime(t_date_time, "%H:%M")).do(close_tournament,
