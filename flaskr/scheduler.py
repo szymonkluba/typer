a6845e771eb68cb7e9d3e7bcc3fa4f54fc273028
Szymon Kluba <szymonkluba@gmail.com>
2020-12-05 00:25:45 +0100

    Corrected wrong time calculation:D

--- flaskr/scheduler.py
+++ flaskr/scheduler.py
@@ -72,13 +72,13 @@ with db_session:
     qualifications = pony_db.select_qualifications_by_date(now)
 with db_session:
     if qualifications:
-        schedule.every().day.at("13:00").do(participants, qualifications=qualifications).tag('checking_participants')
+        schedule.every().day.at("11:00").do(participants, qualifications=qualifications).tag('checking_participants')
         print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking of participants updates', flush=True)
         for i in range(1, 13):
             time_schedule = f'{13 + (i // 4)}:{15 * (i % 4) if i % 4 != 0 else "00"}'
             schedule.every().day.at(time_schedule).do(participants, qualifications=qualifications).tag(
                 'checking_participants')
-t_date_time = current_tournament.date_time + timedelta(hours=1)
+t_date_time = current_tournament.date_time - timedelta(hours=1)
 if now.year == t_date_time.year and now.month == t_date_time.month and now.day == t_date_time.day:
     schedule.every().day.at(datetime.strftime(t_date_time, "%H:%M")).do(close_tournament,
                                                                         tournament=current_tournament,
@@ -92,13 +92,13 @@ if now.year == t_date_time.year and now.month == t_date_time.month and now.day =
         time_finish = t_date_time + timedelta(minutes=10)
         time_finish_S = datetime.strftime(time_finish, "%H:%M")
         schedule.every().day.at(time_finish_S).do(get_results).tag("checking_results")
-schedule.every().day.at("09:00").do(tournament_updates)
+schedule.every().day.at("07:00").do(tournament_updates)
 print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking of tournaments updates', flush=True)
-schedule.every().day.at("09:15").do(new_tournaments)
+schedule.every().day.at("07:15").do(new_tournaments)
 print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking of new tournaments', flush=True)
-schedule.every().day.at("02:00").do(kill_task)
+schedule.every().day.at("01:00").do(kill_task)
 print(f'{datetime.now().strftime("%H:%M")} - Scheduled kill task', flush=True)
-schedule.every().day.at("09:30").do(new_qualifications)
+schedule.every().day.at("07:30").do(new_qualifications)
 print(f'{datetime.now().strftime("%H:%M")} - Scheduled checking new qualifications', flush=True)
 if schedule.jobs:
     for job in schedule.jobs:
