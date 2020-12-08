e4d8514d24a02052be247d432d56a136e3ea84e8
Szymon Kluba <szymonkluba@gmail.com>
2020-12-08 21:55:25 +0100

    Implementation of new points calculation in ranking.

--- flaskr/ranking.py
+++ flaskr/ranking.py
@@ -4,13 +4,33 @@ from flask import (
 
 from flaskr.auth import login_required
 import flaskr.pony_db as pony_db
+from pony.orm import count, sum
+from operator import itemgetter
 
 bp = Blueprint('ranking', __name__)
 
 
-@bp.route('/ranking')
+@bp.route('/ranking/<system>')
 @login_required
-def ranking():
-    users = pony_db.get_users_ranking()
+def ranking(system):
+    points = []
+    users = pony_db.get_users()
+    for user in users:
+        points.append((
+            user,
+            get_points_by_system(user, system),
+            sum(p.times_exact for p in pony_db.Points if p.user == user),
+            pony_db.Points.select(lambda p: p.user == user).count()
+        ))
+    return render_template('ranking/ranking.html',
+                           points=sorted(points, key=itemgetter(1, 2), reverse=True),
+                           system=system)
 
-    return render_template('ranking/ranking.html', users=users)
+
+def get_points_by_system(user, system):
+    return {
+        "classic": sum(p.classic for p in pony_db.Points if p.user == user),
+        "mg": sum(p.mg for p in pony_db.Points if p.user == user),
+        "three_two": sum(p.three_two for p in pony_db.Points if p.user == user),
+        "three_one": sum(p.three_one for p in pony_db.Points if p.user == user)
+    }.get(system)
