507e54c5d20672fb9ab686e09530d79f7240c355
Szymon Kluba <szymonkluba@gmail.com>
2021-01-11 17:37:49 +0100

    Adds information about last points calculation values in ranking table.

--- typer/ranking.py
+++ typer/ranking.py
@@ -4,7 +4,7 @@ from flask import (
 
 from typer.auth import login_required
 import typer.pony_db as pony_db
-from pony.orm import count, sum
+from pony.orm import count, sum, desc
 from operator import itemgetter
 
 bp = Blueprint('ranking', __name__)
@@ -15,12 +15,15 @@ bp = Blueprint('ranking', __name__)
 def ranking(system):
     points = []
     users = pony_db.User.select()
+    last_tournament = pony_db.Tournaments.select(lambda t: t.status == "archiwum").sort_by(desc(pony_db.Tournaments.date_time)).first()
     for user in users:
         points.append((
             user,
             get_points_by_system(user, system),
             sum(p.times_exact for p in pony_db.Points if p.user == user),
-            pony_db.Points.select(lambda p: p.user == user).count()
+            pony_db.Points.select(lambda p: p.user == user).count(),
+            get_last_tournament_points(user, system, last_tournament),
+            sum(p.times_exact for p in pony_db.Points if p.user == user and p.tournament == last_tournament),
         ))
     return render_template('ranking/ranking.html',
                            points=sorted(points, key=itemgetter(1, 2), reverse=True),
@@ -33,4 +36,13 @@ def get_points_by_system(user, system):
         "mg": sum(p.mg for p in pony_db.Points if p.user == user),
         "three_two": sum(p.three_two for p in pony_db.Points if p.user == user),
         "three_one": sum(p.three_one for p in pony_db.Points if p.user == user)
+    }.get(system)
+
+
+def get_last_tournament_points(user, system, last_tournament):
+    return {
+        "classic": sum(p.classic for p in pony_db.Points if p.user == user and p.tournament == last_tournament),
+        "mg": sum(p.mg for p in pony_db.Points if p.user == user and p.tournament == last_tournament),
+        "three_two": sum(p.three_two for p in pony_db.Points if p.user == user and p.tournament == last_tournament),
+        "three_one": sum(p.three_one for p in pony_db.Points if p.user == user and p.tournament == last_tournament)
     }.get(system)
